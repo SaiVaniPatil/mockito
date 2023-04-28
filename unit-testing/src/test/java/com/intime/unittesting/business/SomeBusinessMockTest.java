1e3af0a0da3e5e123aa1dd1bfa25b2f9f1db0b41
SaiVaniPatil <patil.saivani@gmail.com>
2023-04-28 22:18:13 +0200

    mockito changes 2

--- unit-testing/src/test/java/com/intime/unittesting/business/SomeBusinessMockTest.java
+++ unit-testing/src/test/java/com/intime/unittesting/business/SomeBusinessMockTest.java
@@ -1,32 +1,49 @@
 package com.intime.unittesting.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.intime.unittesting.unittesting.business.SomeBusinessImpl;
 import com.intime.unittesting.unittesting.data.SomeDataService;
 
-import static org.mockito.Mockito.mock;
 
-
-
-
+@ExtendWith(MockitoExtension.class)
 public  class SomeBusinessMockTest {
+
+
+    @InjectMocks // will call constructor or setter method
+    SomeBusinessImpl business = new SomeBusinessImpl();
+
+
+    @Mock
+    SomeDataService dataServiceMock;
+    
+    //= mock(SomeDataService.class);
+
+    @BeforeEach
+    public void before()
+    {
+
+       // business.setSomeDataService(dataServiceMock);
+    }
 
     @Test
     public void calculateDataService_basic()
     {
-        SomeBusinessImpl business = new SomeBusinessImpl();
+       // SomeBusinessImpl business = new SomeBusinessImpl();
 
-        SomeDataService mock1 = mock(SomeDataService.class);
+       // SomeDataService mock1 = mock(SomeDataService.class);
 
-        when(mock1.retrieveAllDAta()).thenReturn(new int[]{1,2,3});
+        when(dataServiceMock.retrieveAllDAta()).thenReturn(new int[]{1,2,3});
 
-        business.setSomeDataService(new SomeDataServiceStub());
+        business.setSomeDataService(dataServiceMock);
 
         int actualResult = business.calculateSumUsingDataService();
         int expectedREsult =6;
